d586475a4cb3a32b9dec3251b3fe76e56e34ebed
JosselinEstrada <111148491+JosselinEstrada@users.noreply.github.com>
2023-03-14 12:43:40 -0700

    nana

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var tabProducto = [];
+var codigo = Number;
+var nombre = String;
+var descripcion = String;
+var precio = Boolean;
+var marca = String;
+
+function Product(element){
+    agregar[cod,nom,desc,pre,marc] = tabProducto [
+    {codigo: cod , nombre: nom, descripcion: desc, precio: pre, marca: marc}
+    ]
+}
+function AggProducto(agregar){
+    var agregar= [
+        {
+            agregar : document.getElementById("codigo").value,
+            agregar : document.getElementById("nombre").value,
+            agregar : document.getElementById("descripcion").value,
+            agregar : document.getElementById("precio").value,
+            agregar : document.getElementById("marca").value,
+        }
+    ];
+   alert(agregar);
+   this.agregar.push(Product.insertElementById(agregarProducto));
+
+}
